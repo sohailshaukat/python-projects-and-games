52e5c29ac7f662d0e12422bcf11eb51b3c37f2aa
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-06-21 21:59:33 +0530

    TicTacToe Improved

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
 import random
 def players_info():
-    player1_name=input("Enter the name for player 1:")
-    player2_name=input("Enter the name for player 2:")
-    print(f"Player 1: {player1_name} \nPlayer 2: {player2_name}")
+    player1_name=input("[+]Enter the name for player 1:")
+    player2_name=input("[+]Enter the name for player 2:")
+    print(f"[+]Player 1: {player1_name} \nPlayer 2: {player2_name}")
     return player1_name,player2_name
 
 def first_player_choice(player1_name,player2_name):
-    choice = input("Enter either name of the player to go first or anything else for coin toss: \n")
+    choice = input("[+]Enter either name of the player to go first or anything else for coin toss: \n")
     if choice.lower()==player1_name.lower():
         return True
     elif choice.lower()==player2_name.lower():
         return False
     else:
-        return random.choice([True,False])
+        print("[+]Tossing...")
+        choice_player = random.choice([True,False])
+        if choice:
+            print(f"[+]{player1_name} goes First!")
+        else:
+            print(f"[+]{player2_name} goes First!")
+        return choice_player
 
 def display_grid(game):
     print(game[0]+"|"+game[1]+"|"+game[2])
@@ -25,8 +31,16 @@ def display_grid(game):
 
 def move(grid,sign):
     entry = True
+    dash = "="*10
     while entry:
-        move_index = int(input("Enter the Position"))
+        inp = True
+        while inp:
+            try:
+                move_index = int(input(f"[+]Enter the Position \n {dash} \n"))
+                print(dash)
+                inp = False
+            except ValueError:
+                print("[-]Oops!  That was no valid number.  Try again...")
         if grid[move_index-1] == " " and  1<=move_index<=9 :
             grid[move_index-1] = sign
             entry = False
@@ -58,46 +72,49 @@ def check_victory(grid, sign):
 def victory_message(sign,player1_name,player2_name,is_player_one_first):
     if sign == 'X':
         if is_player_one_first:
-            print(f"{player1_name} Wins!!!")
+            print(f"[+]{player1_name} Wins!!!")
         else:
-            print(f"{player2_name} Wins!!!")
+            print(f"[+]{player2_name} Wins!!!")
     else:
         if is_player_one_first:
-            print(f"{player2_name} Wins!!!")
+            print(f"[+]{player2_name} Wins!!!")
         else:
-            print(f"{player1_name} Wins!!!")
+            print(f"[+]{player1_name} Wins!!!")
     return
 
-print("Welcome to the game!")
-player1_name,player2_name = players_info()
-play = True
-while play:
-    is_player_one_first = first_player_choice(player1_name,player2_name)
-    game = True
-    grid_pos = [str(i) for i in range(1,10)]
-    clear_game = [" "]*9
-    grid = clear_game
-    display_grid(grid_pos)
-    # game = True
-    move_num = 0
-    while game:
-        if move_num%2 == 0:
-            print("For X ", end="")
-            sign = 'X'
-        else:
-            print("For 0 ", end="")
-            sign = '0'
-        grid = move(grid,sign)
-        display_grid(grid)
-        victory = check_victory(grid,sign)
-        if victory:
-            victory_message(sign,player1_name,player2_name,is_player_one_first)
-            break
-        move_num += 1
-        if move_num > 8:
-            print("NO Moves Left!!! It's a DRAW.")
-            game = False
-    play_mode = input("[+]R for Restart. Any key to Quit.")
-    if play_mode.lower() != 'r':
-        print("[-]Quitting...")
-        play = False
+if __name__ == "__main__":
+    print("Welcome to the game!")
+    dash = "="*10
+    player1_name,player2_name = players_info()
+    play = True
+    while play:
+        is_player_one_first = first_player_choice(player1_name,player2_name)
+        game = True
+        grid_pos = [str(i) for i in range(1,10)]
+        clear_game = [" "]*9
+        grid = clear_game
+        display_grid(grid_pos)
+        # game = True
+        move_num = 0
+        while game:
+            if move_num%2 == 0:
+                print("...For X ", end="")
+                sign = 'X'
+            else:
+                print("...For 0 ", end="")
+                sign = '0'
+            grid = move(grid,sign)
+            display_grid(grid)
+            print(dash)
+            victory = check_victory(grid,sign)
+            if victory:
+                victory_message(sign,player1_name,player2_name,is_player_one_first)
+                break
+            move_num += 1
+            if move_num > 8:
+                print("[+]NO Moves Left!!! It's a DRAW.")
+                game = False
+        play_mode = input("[+]R for Restart. Any key to Quit.")
+        if play_mode.lower() != 'r':
+            print("[-]Quitting...")
+            play = False
