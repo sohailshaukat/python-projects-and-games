5b928501d6aa2ea497bba58a6ccaa0753cf26160
Sohail Shaukat Selod <sohail47k@gmail.com>
2019-06-28 19:42:44 +0530

    Create blackjack.py

---
+++ BlackJack/blackjack.py
@@ -0,0 +1,35 @@
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+            'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_contains = ''
+        for card in self.deck:
+            deck_contains += '\n'+card.__str__()
+        return 'The deck contains:' + deck_contains
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
+        
+class Card:
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
